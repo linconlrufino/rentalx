781c93bb5a64fcd1961c27f36114c72048e656a7
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-09-10 23:49:43 -0300

    Listando as categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
@@ -11,6 +11,12 @@ categoriesRoutes.post("/", (request, response) => {
     categoriesRepository.create({ name, description });
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
 });
 
 export { categoriesRoutes };
