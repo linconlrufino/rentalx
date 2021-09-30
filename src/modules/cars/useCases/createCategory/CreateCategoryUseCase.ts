95da1c85c5f0055a855c1901367ce3bea43ef556
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-09-29 21:30:51 -0300

    Configurando Docker e criando Container

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -15,6 +15,7 @@ class CreateCategoryUseCase {
         if (categoryAlreadyExists) {
             throw new Error("Category already exists!");
         }
+
         this.categoriesRepository.create({ name, description });
     }
 }
