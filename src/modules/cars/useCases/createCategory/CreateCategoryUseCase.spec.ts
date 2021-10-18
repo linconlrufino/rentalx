943be55867e4321e6abef7f26290e15e10cab85b
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-10-17 23:20:19 -0300

    Teste de criação de categoria

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,15 +1,5 @@
-describe("Criar categoria", () => {
-    it("Espero que 2 + 2 seja 4", () => {
-        const soma = 2 + 2;
-        const resultado = 4;
-
-        expect(soma).toBe(resultado);
-    });
-
-    it("Espero que  2+2 não seja 5", () => {
-        const soma = 2 + 2;
-        const resultado = 5;
-
-        expect(soma).not.toBe(resultado);
+describe("Create  Category", () => {
+    it("should be  ablet to create a new category", () => {
+        // const createCategory = new CreateCategoryUseCase();
     });
 });
