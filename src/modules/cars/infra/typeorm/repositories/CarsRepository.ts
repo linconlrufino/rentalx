bddf0bca277ab9d9e2cc816dbed64a2810cba8a8
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-11-04 23:21:12 -0300

    Estruturando a entidade de carros

---
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,21 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+
+import { Car } from "../entities/Car";
+
+class CarsRepository implements ICarsRepository {
+    private repository: Repository<Car>;
+
+    constructor() {
+        this.repository = getRepository(Car);
+    }
+
+    create(data: ICreateCarDTO): Promise<Car> {
+        throw new Error("Method not implemented.");
+    }
+    findByLicensePlate(license_plate: string): Promise<Car> {
+        throw new Error("Method not implemented.");
+    }
+}
