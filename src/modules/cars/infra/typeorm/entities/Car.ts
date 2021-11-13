8f3a4dee786d04d3f75e8ce6aa3702f8160660b7
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-11-12 23:55:41 -0300

    cadastro de especificação para carro

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -3,12 +3,15 @@ import {
     CreateDateColumn,
     Entity,
     JoinColumn,
+    JoinTable,
+    ManyToMany,
     ManyToOne,
     PrimaryColumn,
 } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
 import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
@@ -42,6 +45,14 @@ class Car {
 
     @Column()
     category_id: string;
+
+    @ManyToMany(() => Specification)
+    @JoinTable({
+        name: "specifications_cars", // nome da tabela de relacionanento
+        joinColumns: [{ name: "car_id" }], // nome da coluna dentro da nossa tabela de relacionamento que referencia a tabela em uso ( cars no caso)
+        inverseJoinColumns: [{ name: "specification_id" }], // é a outra coluna que referencia a tabela que esta dentro do many to many ( specification)
+    })
+    specifications: Specification[];
 
     @CreateDateColumn()
     created_at: Date;
