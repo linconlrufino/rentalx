3b95ebd2a6b9cd7155052f87cec30282ee1d002e
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-11-21 22:08:47 -0300

    Caso de uso do cadastro de imagens do carro

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,9 +4,11 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
@@ -31,3 +33,9 @@ container.registerSingleton<IUsersRepository>(
 
 // ICarsRepository
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+// IUploadCarImagesRepository
+container.registerSingleton<ICarsImagesRepository>(
+    "CarsImagesRepository",
+    CarsImagesRepository,
+);
