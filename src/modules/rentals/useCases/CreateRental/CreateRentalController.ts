6489b8bbea7b09e3a8d922c103226d3ac7e14dea
Linconl Rufino <linconldasilvarufino@gmail.com>
2021-12-05 23:57:15 -0300

    Criando controller

--- src/modules/rentals/useCases/CreateRental/CreateRentalController.ts
+++ src/modules/rentals/useCases/CreateRental/CreateRentalController.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 class CreateRentalController {
     async handle(request: Request, response: Response): Promise<Response> {
-        return response.send();
+        const createRentalUseCase = container.resolve(CreateRentalUseCase);
+
+        const { id } = request.user;
+        const { car_id, expected_return_date } = request.body;
+
+        const rental = await createRentalUseCase.execute({
+            car_id,
+            expected_return_date,
+            user_id: id,
+        });
+
+        return response.status(201).json(rental);
     }
 }
 
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,8 @@ import "reflect-metadata";
 
 import { container } from "tsyringe";
 
+import "@shared/container/providers";
+
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
@@ -12,6 +14,8 @@ import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRep
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -38,4 +42,10 @@ container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 container.registerSingleton<ICarsImagesRepository>(
     "CarsImagesRepository",
     CarsImagesRepository,
+);
+
+// IRentalRepository
+container.registerSingleton<IRentalsRepository>(
+    "RentalsRepository",
+    RentalsRepository,
 );
